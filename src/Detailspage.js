7637961c4903091ab1c152f36665eaa51c670f54
Nithish <snithishkumar9@gmail.com>
2021-07-05 19:32:11 +0530

    detailspage changes and removed unwanted code

--- src/Detailspage.js
+++ src/Detailspage.js
@@ -15,25 +15,41 @@ const useStyles = makeStyles(() => ({
 export default function Detailspage(props) {
     const classes = useStyles();
     const Documenu = require('documenu')
-Documenu.configure('0a87f7cafb3987be18bff1ba0815d561')
-const [resultData, setResultData] = useState([])
-
-useEffect(() => {
-  async function fetchMyAPI() {
-    let response = await  Documenu.Restaurants.getMenuItems(restaurantid)
-    setResultData(response.data)
-  }
-  fetchMyAPI()
-}, [])
-
+    Documenu.configure('0a87f7cafb3987be18bff1ba0815d561')
     let restaurant_id = props.location.state.restaurantid
     let restaurentname = props.location.state.restaurentname
-    var restaurantid = restaurant_id.toString();
-    console.log("restaurantid",restaurantid)
-    console.log("resultData",resultData)
-
-
-    // const D = (props) => <Typography style={{ fontWeight: 'bold', fontSize: 16, color: 'black' }}>{props.children}</Typography>
+    var data = [
+        {
+            menu_item_name: "Hard tacos or tostadas.",
+            menu_item_price: "250"
+        },
+        {
+            menu_item_name: "Huevos rancheros.",
+            menu_item_price: "320"
+        },
+        {
+            menu_item_name: "Stir fry.",
+            menu_item_price: "1114"
+        },
+        {
+            menu_item_name: "Burgers or hotdogs.",
+            menu_item_price: "789"
+        },
+        {
+            menu_item_name: "Quesadillas (plain cheese, steak, chicken, or pork)",
+            menu_item_price: "1254"
+        },
+    ]
+    const [resultData, setResultData] = useState(data)
+    useEffect(() => {
+        async function fetchMyAPI() {
+            let response = await Documenu.Restaurants.getMenuItems(restaurant_id)
+            if (response.data.length > 0) {
+                setResultData(response.data)
+            }
+        }
+        fetchMyAPI()
+    }, [])
     return (
         <Grid
             container
@@ -44,26 +60,14 @@ useEffect(() => {
             <Grid item xs={3} lg={6}>
                 <Card className={classes.cardstyles}>
                     <CardContent>
-                    <Typography align='center'><h2>{restaurentname}</h2></Typography>
+                        <Typography align='center'><h2>{restaurentname}</h2></Typography>
                         <Typography align='center'><h2>Menu</h2></Typography>
-                    <Typography style={{marginBottom:'10px'}}><b>Vegiterian</b></Typography>
-                        {/* {vegData.map((value) => {
-                            return (
-                                <div style={{marginBottom:'7px'}}>
-                                    <Typography>{value.name} </Typography>
-                                    <Typography align='right' style={{ marginTop: '-25px' }}><b> {value.price}</b></Typography>
-                                </div>
-                            )
-                        })} */}
-                        <Typography style={{marginBottom:'10px'}}><b>Non-Vegiterian</b></Typography>
-                        {/* {nonVegData.map((value) => {
-                            return (
-                                <div style={{marginBottom:'7px'}}>
-                                    <Typography>{value.name} </Typography>
-                                    <Typography align='right' style={{ marginTop: '-25px' }}> <b>{value.price}</b></Typography>
-                                </div>
-                            )
-                        })} */}
+                        {resultData.map((value) => (
+                            <div style={{ marginBottom: '7px' }}>
+                                <Typography>{value.menu_item_name} </Typography>
+                                <Typography align='right' style={{ marginTop: '-25px' }}><b>$ {value.menu_item_price}</b></Typography>
+                            </div>
+                        ))}
                     </CardContent>
                 </Card>
             </Grid>
